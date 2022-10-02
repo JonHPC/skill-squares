c0f6d1e93e207353bcbda28d637f279cf9f3c738
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-10-01 20:56:27 -0700

    Worked on Squares

--- src/client/components/Square.jsx
+++ src/client/components/Square.jsx
@@ -5,14 +5,20 @@ function Square(props) {
 
     const [skill, setSkill] = useState(props.skill)
     const [isGreen, setIsGreen] = useState(skill.green)
+    const [skillId, setSkillId] = useState(skill.id)
+
+    const handleClick = () => {
+      props.toggleSquare(skillId)
+    }
+
     return (
-      <div className="square w-32 h-32 overflow-auto">
+      <div onClick={handleClick} className="square w-32 h-32 overflow-auto transition active:scale-95">
         {isGreen ?
-          <div className="text-gray-800 flex justify-center items-center bg-emerald-400 h-full w-full transition hover:bg-emerald-300 hover:cursor-pointer active:bg-emerald-500">
+          <div className="text-gray-800 flex justify-center items-center bg-emerald-400 h-full w-full transition hover:bg-emerald-300 hover:cursor-pointer active:bg-emerald-400">
             {skill.text}
           </div>
           :
-          <div className="flex justify-center items-center bg-gray-700 h-full w-full transition hover:bg-gray-600 hover:cursor-pointer active:bg-gray-800">
+          <div className="flex justify-center items-center bg-gray-700 h-full w-full transition hover:bg-gray-600 hover:cursor-pointer active:bg-gray-700">
             {skill.text}
           </div>
         }
--- src/client/components/SquareContainer.jsx
+++ src/client/components/SquareContainer.jsx
@@ -6,18 +6,43 @@ import Square from './Square'
 function SquareContainer() {
 
   const [skills, setSkills] = useState([])
-  
+
   useEffect(() => {
+    fetchAllSkills()
+  },[])
+
+  const fetchAllSkills = () => {
     axios.get('/skills')
       .then(response => {
         const rows = []
         response.data.map(el => {
-          rows.push(<Square key={el.id} id={el.id} skill={el} />)
+          rows.push(<Square toggleSquare={toggleSquare} id={el.id} key={el.id} skill={el} />)
         })
+        console.log('fetchAllSkills rows: ', rows)
         setSkills(rows)
       })
-  }, [])
+  }
 
+  async function fetchSkill(id){
+    const res = await axios.get(`skills/${id}`)
+    return res.data
+  }
+
+  async function toggleSquare(id){
+    
+    const skillToToggle = await fetchSkill(id)
+    console.log('toggleSquare skillToToggle: ', skillToToggle)
+    const payload = {...skillToToggle, green: !skillToToggle.green}
+    axios.patch(`skills/${id}`, payload)
+      .then(res => {
+        const data = res.data
+        
+        fetchAllSkills()
+      })
+      //Might need to add another child component to pass the array to, that child can then
+      //iterate through the array to render the components
+  }
+  
   return (
     <div className="square-container flex flex-wrap justify-center items-center gap-2 bg-gray-900 p-4">
       {skills}
