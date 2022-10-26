9bb8993f5ac12b8ef06358e9dc67f593eb077dd8
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-10-26 14:33:15 -0700

    Basic login component

---
+++ src/client/components/Login.jsx
@@ -0,0 +1,19 @@
+//src/client/components/Login.jsx
+
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function Login() {
+    
+
+    
+
+    return (
+      <div className="login">
+        
+      </div>
+    )
+  }
+  
+  export default Login
+  
