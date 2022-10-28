bcb1c8171bf51f48885244a64d6c6b1401a508d0
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-10-28 09:33:15 -0700

    Moved connection string to .env

--- src/server/models/skillsModel.js
+++ src/server/models/skillsModel.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import { isDepsOptimizerEnabled } from 'vite'
 const Schema = mongoose.Schema
+import dotenv from 'dotenv'
+dotenv.config()
 
-const MONGO_URI = 'mongodb+srv://skill-squares:ImproveSkills1234@cluster0.uhu1iyu.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uhu1iyu.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.connection.once('open', () => console.log('Connected to MongoDB'))
